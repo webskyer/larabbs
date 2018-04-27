31b688b7980860dee37e2cda591f65fa775a4c1c
vagrant <vagrant@homestead>
2018-04-27 06:16:32 +0000

    user can upload topic image

--- routes/web.php
+++ routes/web.php
@@ -33,4 +33,8 @@ Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit'
 
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
 
+// Categpry Routes...
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
+
+// Upload image Routes
+Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
