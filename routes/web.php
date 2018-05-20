8b43b98cf10c73c2023c709fe3cb2b3f55743f6d
vagrant <vagrant@homestead>
2018-05-20 03:39:16 +0000

    User can create reply

--- routes/web.php
+++ routes/web.php
@@ -41,7 +41,5 @@ Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 // Upload image Routes
 Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
 
-
-
-
-Route::resource('replies', 'RepliesController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+// Replies Routes
+Route::resource('replies', 'RepliesController', ['only' => ['store', 'destroy']]);
